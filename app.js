e3c2e6c1a8d567a40dd335f400b1415fe0a7556d
Fernando <fernandoagro.654@gmail.com>
2021-09-29 20:01:10 -0300

    Comando importantes do Git

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log("Alteração de Dados");
+console.log("----- Comandos Importantes -----");
+console.log("git commit -m mais o nome da alteração");
+console.log("git status - ver o status do repositorio");
+console.log("git init - cria o repositorio local");
+console.log("git config user.name - adiciona o nome do usuario");
+console.log("git config user.email - adiciona o email do usuario");
+console.log("git push origin master - manda a documentação para o repositorio remoto");
+console.log("git remote add + a url - adiciona o repositorio remoto");
